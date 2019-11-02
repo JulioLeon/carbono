608fe334543ecb6d0b0663378303a28b52252990
Julio Cesar <trullin1@gmail.com>
2019-11-02 15:11:03 -0500

    modelo comprobantes

--- application/models/Comprobantes_model.php
+++ application/models/Comprobantes_model.php
@@ -8,7 +8,7 @@ class Comprobantes_model extends CI_Model {
 	{
 
         $opc = 1;
-        $query = $this->db->query(" CALL SP_SUCURSAL2($opc,'','','','',@outcomprobantes); ");
+        $query = $this->db->query(" CALL SP_COMPROBANTES($opc,'','','','',@outcomprobantes); ");
         //$query = $this->db->query("Select @outcomprobantes  as mensaje;"); 
         return $query->result(); 
     }    
@@ -17,14 +17,14 @@ class Comprobantes_model extends CI_Model {
     {
         $opc = 5;
         $estado = "1";
-        $query = $this->db->query(" CALL SP_SUCURSAL2($opc, '','','','".$estado."',@outcomprobantes);");
+        $query = $this->db->query(" CALL SP_COMPROBANTES($opc, '','','','".$estado."',@outcomprobantes);");
         return $query->result();
     }
 
     public function graba_comprobantes($codigo,$nombre,$tienda,$estado)
     {
         $opc = 2;        
-        $this->db->query(" CALL SP_SUCURSAL2($opc, '".$codigo."','".$nombre."','".$tienda."','".$estado."',@outcomprobantes);");
+        $this->db->query(" CALL SP_COMPROBANTES($opc, '".$codigo."','".$nombre."','".$tienda."','".$estado."',@outcomprobantes);");
         $query = $this->db->query("SELECT @outcomprobantes as rpta;");
         return $query->result();
     }
@@ -32,7 +32,7 @@ class Comprobantes_model extends CI_Model {
     public function actualiza_comprobantes($codigo,$nombre,$tienda,$estado)
     {
         $opc = 3;
-        $this->db->query(" CALL SP_SUCURSAL2($opc, '".$codigo."','".$nombre."','".$tienda."','".$estado."',@outcomprobantes);");
+        $this->db->query(" CALL SP_COMPROBANTES($opc, '".$codigo."','".$nombre."','".$tienda."','".$estado."',@outcomprobantes);");
         $query = $this->db->query("SELECT @outcomprobantes as rpta;");
         return $query->result();
     }
